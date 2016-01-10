2a2be9d51ceb8ab647fa938d0c3e686ae58cc04e
root <jmacias@solidgear.es>
2016-01-10 10:01:13 +0000

    First generation

--- generate_bulletin.php
+++ generate_bulletin.php
@@ -2,21 +2,24 @@
 
 <?php
 
-if (!isset($argv[1])) {
-    echo "Usage:\n\tgenerate_bulletin.php <SOURCE_FOLDER>".PHP_EOL;
+if (!isset($argv[1]) || !isset($argv[2])) {
+    echo "Usage:\n\tgenerate_bulletin.php <SOURCE_FOLDER> <CREATE_THUMBNAILS true|false>".PHP_EOL;
     die();
 }
 
 include_once './lib/bulletin.php';
 
 try{
-    $bulletin = new \SG\bulletin\lib\Bulletin($argv[1]);
+    $bulletin = new \SG\bulletin\lib\Bulletin($argv[1], $argv[2]);
 }catch (\Exception $e){
     echo "Exception".$e->getMessage().PHP_EOL;
 }
 
-echo $bulletin->getHTML();
+$output = $bulletin->getHTML();
 
+file_put_contents ($argv[1]."index.html", $output);
+
+echo "Done".PHP_EOL;
 //TODO: Render HTML
 //TODO: Create ZIP with content
 //TODO: Generate PDF
